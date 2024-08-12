bb996561e485a566cd86e39c139a233e8e42fc33
Khadeeja beevi <khadeejabeevinavas@gmail.com>
2024-08-12 22:45:33 +0530

    ok

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//typing animation
+var typed=new Typed(".typing",{
+    strings:["MCA Student","","Web Developer","Web Designer","FrontEnd Developer","Freelancer","BackEnd Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
